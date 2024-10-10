432b8bc0aa8574ac8b082c09c8bb156283c9b2d6
masterj <masterj122517@163.com>
2024-10-10 21:56:22 +0800

    config font

--- statusbar/packages/bat.sh
+++ statusbar/packages/bat.sh
@@ -20,27 +20,19 @@ update() {
   [ ! "$bat_text" ] && bat_text=$(acpi -b | sed 2d | awk '{print $5}' | grep -Eo "[0-9]+")
   [ ! "$(acpi -b | grep 'Battery 0' | grep Discharging)" ] && charge_icon=" "
   if [ "$bat_text" -ge 95 ]; then
-    bat_icon=""
+    bat_icon="" # 满电
     charge_icon=""
   elif [ "$bat_text" -ge 90 ]; then
-    bat_icon=""
-  elif [ "$bat_text" -ge 80 ]; then
-    bat_icon=""
+    bat_icon="" # 90%+
   elif [ "$bat_text" -ge 70 ]; then
-    bat_icon=""
-  elif [ "$bat_text" -ge 60 ]; then
-    bat_icon=""
-  elif [ "$bat_text" -ge 50 ]; then
-    bat_icon=""
+    bat_icon="" # 70%+
   elif [ "$bat_text" -ge 40 ]; then
-    bat_icon=""
-  elif [ "$bat_text" -ge 30 ]; then
-    bat_icon=""
+    bat_icon="" # 40%+
   elif [ "$bat_text" -ge 20 ]; then
-    bat_icon=""
-  elif [ "$bat_text" -ge 10 ]; then
-    bat_icon=""
-  else bat_icon=""; fi
+    bat_icon="" # 20%+
+  else
+    bat_icon="" # 低电量
+  fi
 
   icon=" $charge_icon$bat_icon "
   text=" $bat_text% "
